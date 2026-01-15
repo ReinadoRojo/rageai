0b6f96b4cc43fd6f9e365585fa0d6c3708e72449
Ezequiel C. <62150172+ReinadoRojo@users.noreply.github.com>
2026-01-15 12:29:05 +0000

    server identification

--- lib/umami.ts
+++ lib/umami.ts
@@ -9,28 +9,6 @@ umami.init({
   hostUrl: 'https://cloud.umami.is',
 });
 
-async function serverTrack(data?: object) {
-  try {
-    const response = await fetch("https://api-gateway.umami.dev/api/send", {
-      method: "POST",
-      body: JSON.stringify({
-        "type":"event",
-        "payload": {
-          "website":"7839702f-8d3e-483b-8c15-ecc3da1d16e1",
-          "language":"en-US",
-          "title":"RageAI",
-          "hostname":"rageai-seven.vercel.app",
-          "url":"https://rageai-seven.vercel.app/",
-          "referrer":"",
-          "id": "server:reserved",
-          data
-        }
-      })
-    })
-    return response
-  } catch(e) {
-    throw e;
-  }
-}
+umami.identify({ name: "server:reserved" })
 
-export { umami, serverTrack };
+export { umami };
